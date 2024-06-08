896e7fd92f9048135d688b2e7446c521dfc3b97b
nhutch127 <nathan17@illinois.edu>
2024-06-07 19:37:10 -0500

    cleanup

--- Kalman/main.py
+++ Kalman/main.py
@@ -2,18 +2,12 @@ import numpy as np
 from Equations import Kalman
 import matplotlib.pyplot as plt
 
-A = np.array([[1, 1],
-                  [0, 1]])
-B = np.array([[0.5],
-                [1]])
+A = np.array([[1, 1], [0, 1]])
 H = np.array([[1, 0]])
-Q = np.array([[0.1, 0],
-                [0, 0.1]])
+Q = np.array([[0.1, 0], [0, 0.1]])
 R = np.array([[0.1]])
-P = np.array([[1, 0],
-                [0, 1]])
-x0 = np.array([[0],
-                [0]]) #this is gonna have 16 elements instead of 2 lol
+P = np.array([[1, 0], [0, 1]])
+x0 = np.array([[0], [0]]) #this is gonna have 16 elements instead of 2 lol
 
 kf = Kalman(A, H, Q, R, P, x0)
 measurements = [1, 2, 3, 4, 5]
